0899873ba8e31617df8e05514b999a06bbc0d8ef
jianguo <1060684755@qq.com>
2019-09-19 17:06:36 +0800

    feat: 安装prettier package，调整eslint代码规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,24 @@
 module.exports = {
-	root: true,
-	parser: 'babel-eslint',
-	parserOptions: {
-		sourceType: 'module'
-	},
-	env: {
-		browser: true,
-	},
-	extends: 'standard',
-	plugins: [
-		'html'
-	],
-	'rules': {
-		'semi': 0,
-		'quotes': 0,
-		'no-new': 0,
-		'no-var': 2,
-		'eol-last': 0,
-		'brace-style': 0,
-		'prefer-const': 2,
-		'comma-dangle': 2,      // 禁止结尾有多余的逗号
-		'no-unused-vars': 1,
-		'spaced-comment': 0,    // 要求或禁止在注释前有空白
-		'no-multi-spaces': 0,
-		'keyword-spacing': 0,
-		'indent': ['error', 2],
-		'no-useless-return': 0,
-		'no-mixed-operators': 0,
-		'space-before-function-paren': 0,
-	}
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018
+  },
+  env: {
+    browser: true,
+    es6: true
+  },
+  'rules': {
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/explicit-function-return-type': ['error', {
+      allowExpressions: true
+    }],
+    '@typescript-eslint/interface-name-prefix': 'off'
+  }
 }
